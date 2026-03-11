b6495a893f6d12fb36708bf9767ab966d80b2d91
JaimeVGA <cortesvegajaimefabian@gmail.com>
2026-03-11 16:12:55 -0600

    Modulo de ensamblador que permite copiar cadenas al revez

--- Cadenas/strcpyRevez.asm
+++ Cadenas/strcpyRevez.asm
@@ -1,17 +1,28 @@
-section .text
+section .text 
     global mi_strcpy
 
 mi_strcpy:
-    ;rdi = destino donde compiar
-    ;rsi = origen de donde leer
+    ; rdi = destino
+    ; rsi = origen
 
-    mov al,0 
+    mov r8, rdi             ; guardar destino
+   
+    ; calcular longitud de origen con SCASB
+    mov rdi, rsi            
+    xor al, al              
+    mov rcx, -1
+    cld
+    repnz scasb             ; repite hasta encontrar AL
 
-.bucle:
-    add rsi, [rsi + 1]
-    cmp [rsi-1],al
-    jne .bucle 
-
-    mov byte [rdi],rsi      
-
+    sub rdi, rsi            ; resta para obtener longitud de la cadena (incluyendo '\0')
+    mov rcx,2               ; restamos 2 para obtener la longitud real sin el caracter nulo
+    sub rdi, rcx            ; ajustar para obtener la longitud real sin el caracter nulo
+    
+.copiar:            
+    mov al, [rsi + rdi]     ; cargar byte desde origen (empezando por el final)
+    mov [r8], al            ; escribir byte en destino
+    inc r8 
+    dec rdi                 ; decrementar contador
+    cmp rdi, -1
+    jne .copiar             ; repetir hasta copiar toda la cadena
     ret
